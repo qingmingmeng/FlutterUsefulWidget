dd09e84beb92fa88d96d4b5a8d767b344e72a8e7
zhangjie <qingmingmeng0@gmail.com>
2019-12-30 15:02:30 +0800

    页面增加AppBar

--- lib/right_back/right_back.dart
+++ lib/right_back/right_back.dart
@@ -4,6 +4,10 @@ class RightBack extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('右滑返回'),
+        centerTitle: true,
+      ),
       body: Center(
         child: Text('右滑返回上一页',style: TextStyle(color: Colors.greenAccent,fontSize: 28.0),),
       ),
