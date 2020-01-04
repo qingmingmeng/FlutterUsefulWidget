25d7030449e4c35fd09a31c4114219bb4d4a6ef0
zhangjie <qingmingmeng0@gmail.com>
2020-01-04 15:34:51 +0800

    主页增加双击退出

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,132 +11,166 @@ import 'package:flutter_actual/progress/progress.dart';
 import 'package:flutter_actual/right_back/right_back.dart';
 import 'package:flutter_actual/search/search.dart';
 import 'package:flutter_actual/wrap/wrap.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import 'bessel/bessel.dart';
 import 'expansion/expansion_panel_list.dart';
 import 'expansion/expansion_tile.dart';
 import 'keep_state/keep_state.dart';
 
-class MainHome extends StatelessWidget{
+class MainHome extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter 实战'),
-        centerTitle: true,
-        elevation: 0.0,//阴影效果，0.0标识没有阴影
-      ),
-      drawer: MyDrawer(),//左滑抽屉
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text('底部导航栏1'),
-              onTap: () => _pageJumpWithRightBack(context, BottomGuide1()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('底部导航栏2'),
-              onTap: () => _pageJumpWithRightBack(context, BottomGuide2()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('跳转动画'),
-              onTap: () => _pageJumpWithRightBack(context, AnimationList()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('磨砂玻璃效果'),
-              onTap: () => _pageJumpWithRightBack(context, FrostedGlass()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('TabBar + 保持页面状态'),
-              onTap: () => _pageJumpWithRightBack(context, KeepState()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('搜索框'),
-              onTap: () => _pageJumpWithRightBack(context, Search()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('Wrap流式布局'),
-              onTap: () => _pageJumpWithRightBack(context, WrapWidget()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('展开闭合(ExpansionTile)'),
-              onTap: () => _pageJumpWithRightBack(context, ExpansionTileWidget()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('展开闭合(ExpansionPanelList)'),
-              onTap: () => _pageJumpWithRightBack(context, ExpansionPanelListWidget()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('路径切割(贝塞尔曲线)'),
-              onTap: () => _pageJumpWithRightBack(context, BesselWidget()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('右滑返回'),
-              onTap: () => _pageJumpWithRightBack(context,RightBack()),
-            ),
-          ),
-          Card(
-            child: Tooltip(
-              message: '这是一个轻量级提示',
-              child: ListTile(
-                title: Text('长按轻量级提示'),
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('拖动效果'),
-              onTap: () => _pageJumpWithRightBack(context,Drag()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('数据保存与读取'),
-              onTap: () => _pageJumpWithRightBack(context,DataSaveAndRead()),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('进度条'),
-              onTap: () => _pageJumpWithRightBack(context,Progress()),
-            ),
-          ),
-        ],
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return _MainHomeState();
   }
 }
 
-//普通的页面跳转
-_pageJump(BuildContext context, Widget widget){
-  Navigator.push(context, MaterialPageRoute(
-      builder: (context) => widget,
-  ));
-}
+class _MainHomeState extends State<MainHome>{
+  DateTime _lastPressedAt; //上次点击时间
 
-//页面跳转，且可以右滑返回，注意，需要从屏幕最左侧滑动才有效果
-_pageJumpWithRightBack(BuildContext context, Widget widget){
-  Navigator.of(context).push(CupertinoPageRoute(
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _doubleExit,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter 实战'),
+          centerTitle: true,
+          elevation: 0.0,//阴影效果，0.0标识没有阴影
+        ),
+        drawer: MyDrawer(),//左滑抽屉
+        body: ListView(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                title: Text('底部导航栏1'),
+                onTap: () => _pageJumpWithRightBack(context, BottomGuide1()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('底部导航栏2'),
+                onTap: () => _pageJumpWithRightBack(context, BottomGuide2()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('跳转动画'),
+                onTap: () => _pageJumpWithRightBack(context, AnimationList()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('磨砂玻璃效果'),
+                onTap: () => _pageJumpWithRightBack(context, FrostedGlass()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('TabBar + 保持页面状态'),
+                onTap: () => _pageJumpWithRightBack(context, KeepState()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('搜索框'),
+                onTap: () => _pageJumpWithRightBack(context, Search()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('Wrap流式布局'),
+                onTap: () => _pageJumpWithRightBack(context, WrapWidget()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('展开闭合(ExpansionTile)'),
+                onTap: () => _pageJumpWithRightBack(context, ExpansionTileWidget()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('展开闭合(ExpansionPanelList)'),
+                onTap: () => _pageJumpWithRightBack(context, ExpansionPanelListWidget()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('路径切割(贝塞尔曲线)'),
+                onTap: () => _pageJumpWithRightBack(context, BesselWidget()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('右滑返回'),
+                onTap: () => _pageJumpWithRightBack(context,RightBack()),
+              ),
+            ),
+            Card(
+              child: Tooltip(
+                message: '这是一个轻量级提示',
+                child: ListTile(
+                  title: Text('长按轻量级提示'),
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('拖动效果'),
+                onTap: () => _pageJumpWithRightBack(context,Drag()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('数据保存与读取'),
+                onTap: () => _pageJumpWithRightBack(context,DataSaveAndRead()),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('进度条'),
+                onTap: () => _pageJumpWithRightBack(context,Progress()),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //普通的页面跳转
+  _pageJump(BuildContext context, Widget widget){
+    Navigator.push(context, MaterialPageRoute(
       builder: (context) => widget,
-  ));
+    ));
+  }
+
+  //页面跳转，且可以右滑返回，注意，需要从屏幕最左侧滑动才有效果
+  _pageJumpWithRightBack(BuildContext context, Widget widget){
+    Navigator.of(context).push(CupertinoPageRoute(
+      builder: (context) => widget,
+    ));
+  }
+
+  //toast提示
+  _showToast(String string){
+    Fluttertoast.showToast(
+      msg: string,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+    );
+  }
+
+  //双击退出
+  Future<bool> _doubleExit(){
+    if(_lastPressedAt == null ||
+        DateTime.now().difference(_lastPressedAt) > Duration(seconds: 1)){
+      //两次点击间隔超过1秒则重新计时
+      _lastPressedAt = DateTime.now();
+      _showToast('双击退出');
+      return Future.value(false);
+    }
+    return Future.value(true);
+  }
 }
