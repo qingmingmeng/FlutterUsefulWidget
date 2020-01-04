83b4827d1413fa2bb7a4f9d67de041d46ee0600c
zhangjie <qingmingmeng0@gmail.com>
2020-01-04 17:27:47 +0800

    动态更新进度条

--- lib/progress/progress.dart
+++ lib/progress/progress.dart
@@ -1,6 +1,25 @@
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class Progress extends StatelessWidget {
+class Progress extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ProgressState();
+  }
+}
+
+class ProgressState extends State<Progress>{
+  Timer _timer;
+  double _value = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _startCountdownTimer();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +46,7 @@ class Progress extends StatelessWidget {
               child: LinearProgressIndicator(
                 backgroundColor: Colors.grey[200],
                 valueColor: AlwaysStoppedAnimation(Colors.blue),
-                value: 0.7,
+                value: _value,
               ),
             ),
           ),
@@ -52,12 +71,34 @@ class Progress extends StatelessWidget {
                 backgroundColor: Colors.grey[200],
                 valueColor: AlwaysStoppedAnimation(Colors.blue),
                 strokeWidth: 4,
-                value: 0.70,
+                value: _value,
               ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void _startCountdownTimer() {
+    const oneSec = const Duration(milliseconds: 10);
+    var callback = (timer) => {
+      setState(() {
+        if(_value < 1){
+          _value += 0.001;
+        } else {
+          _timer.cancel();
+        }
+      })
+    };
+    _timer = Timer.periodic(oneSec, callback);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if(null != _timer){
+      _timer.cancel();
+    }
   }
 }
