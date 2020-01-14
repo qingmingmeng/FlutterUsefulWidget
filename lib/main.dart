330ba8d4cd51a94d2257a0a8faff421e8b2613ad
zhangjie <qingmingmeng0@gmail.com>
2020-01-14 14:54:33 +0800

    启动页修改延迟跳转的方式，普通跳转，不使用动画

--- lib/main.dart
+++ lib/main.dart
@@ -21,40 +21,49 @@ class SplashScreen extends StatefulWidget{
 }
 
 class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
-  AnimationController _controller;
-  Animation _animation;
+//  AnimationController _controller;
+//  Animation _animation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 2000));
-    _animation = Tween(begin: 1.0, end: 1.0).animate(_controller);
-
-    _animation.addStatusListener((states){
-      if(states == AnimationStatus.completed){//动画结束
-        Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (context) => MainHome()),
-                (route) => route == null);
-      }
+    Future.delayed(Duration(seconds: 1),(){
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => MainHome()
+      ));
     });
-
-    _controller.forward();//播放动画
+//    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 2000));
+//    _animation = Tween(begin: 1.0, end: 1.0).animate(_controller);
+//
+//    _animation.addStatusListener((states){
+//      if(states == AnimationStatus.completed){//动画结束
+//        Navigator.of(context).pushAndRemoveUntil(
+//            MaterialPageRoute(builder: (context) => MainHome()),
+//                (route) => route == null);
+//      }
+//    });
+//
+//    _controller.forward();//播放动画
   }
 
   @override
   Widget build(BuildContext context) {
-    return FadeTransition(
-      opacity: _animation,
-      child: Image.asset(
-        'images/splash.jpg',
-        fit: BoxFit.cover,
-      ),
+    return Image.asset(
+      'images/splash.jpg',
+      fit: BoxFit.cover,
     );
+//    return FadeTransition(
+//      opacity: _animation,
+//      child: Image.asset(
+//        'images/splash.jpg',
+//        fit: BoxFit.cover,
+//      ),
+//    );
   }
 
   @override
   void dispose() {
     super.dispose();
-    _controller.dispose();
+//    _controller.dispose();
   }
 }
